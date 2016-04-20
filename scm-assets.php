6320fd3db2c741a6b57bc8ba66751b1a73c0bcec
MdMbunny <info@mdmbunny.com>
2016-04-20 11:58:27 +0200

    v1.0.4

--- scm-assets.php
+++ scm-assets.php
@@ -3,7 +3,7 @@
  * Plugin Name:         SCM Assets
  * Plugin URI:          http://studiocreativo-m.it/
  * Description:         SCM Javascript Integration
- * Version:             1.0.3
+ * Version:             1.0.4
  * Author:              Studio Creativo M
  * Author URI:          http://studiocreativo-m.it/
  * License:             http://www.gnu.org/licenses/gpl-3.0.html
@@ -15,7 +15,8 @@
 *****************************************************
 *
 *   0.0 Actions and Filters
-*   1.0 Assets
+*   1.0 Init
+*   2.0 Assets
 *
 *****************************************************
 */
@@ -24,22 +25,32 @@
 // *      0.0 ACTIONS AND FILTERS
 // *****************************************************
 
+    add_action( 'plugins_loaded', 'scm_assets_init' );
+
     add_action( 'admin_enqueue_scripts', 'scm_assets_admin_register' );
     add_action( 'login_enqueue_scripts', 'scm_assets_login_register' );
     add_action( 'wp_enqueue_scripts', 'scm_assets_register_styles' );
-    add_action( 'wp_enqueue_scripts', 'scm_assets_register_scripts' );
-    
-    add_action( 'plugins_loaded', 'scm_assets_init' );
-
-
+    add_action( 'wp_enqueue_scripts', 'scm_assets_register_scripts' );    
 
 // *****************************************************
-// *      1.0 ASSETS
+// *      1.0 INIT
 // *****************************************************
+
+    if ( ! function_exists( 'scm_assets_version' ) ) {
+        function scm_assets_version() {
+            if ( ! function_exists( 'get_plugins' ) )
+                require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+            $plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
+            $plugin_file = basename( ( __FILE__ ) );
+            return $plugin_folder[$plugin_file]['Version'];
+        }
+    }
+
 
     if ( ! function_exists( 'scm_assets_init' ) ) {
         function scm_assets_init() {
             
+            define( 'SCM_ASSETS_VERSION',                scm_assets_version() );
             define( 'SCM_ASSETS_DIR',                    dirname(__FILE__) . '/' );
             define( 'SCM_ASSETS_URI',                    plugin_dir_url(__FILE__) );
             define( 'SCM_ASSETS_DIR_ASSETS',             SCM_ASSETS_DIR . 'assets/' );
@@ -48,13 +59,17 @@
         }
     }
 
-        //styles
+// *****************************************************
+// *      2.0 ASSETS
+// *****************************************************
+
+    //styles
     if ( ! function_exists( 'scm_assets_register_styles' ) ) {
         function scm_assets_register_styles() {
 
             // Fancybox
             
-            if( scm_field( 'opt-tools-fancybox', 0, 'option' ) ){
+            if( get_field( 'opt-tools-fancybox', 'option' ) ){
                 wp_register_style( 'fancybox-style', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/jquery.fancybox.css', false, null );
                 wp_register_style( 'fancybox-thumbs-style', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/helpers/jquery.fancybox-thumbs.css', false, null );
                 wp_register_style( 'fancybox-buttons-style', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/helpers/jquery.fancybox-buttons.css', false, null );
@@ -78,33 +93,25 @@
                 wp_register_style( 'bx-style', SCM_ASSETS_URI_ASSETS . 'jquery.bxslider-4.1.2/jquery.bxslider.css', false, null );
                 wp_enqueue_style( 'bx-style' );
             }
-
-            // Parallax
-
-            /*if( scm_field( 'opt-tools-parallax', 0, 'option' ) ){
-                wp_register_style( 'parallax', SCM_ASSETS_URI_ASSETS . 'parallax-1.3.1/parallax.css', false, SCM_SCRIPTS_VERSION );
-                wp_enqueue_style( 'parallax' );
-            }*/
-
         }
     }
 
-
+    // scripts
     if ( ! function_exists( 'scm_assets_register_scripts' ) ) {
         function scm_assets_register_scripts() {
 
             // SCM Stuff
             
-            wp_register_script( 'jquery-scm-presets', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.presets.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, false );
+            wp_register_script( 'jquery-scm-presets', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.presets.js', array( 'jquery' ), null, false );
             wp_enqueue_script( 'jquery-scm-presets' );
 
-            wp_register_script( 'jquery-scm-functions', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.functions.js', array( 'jquery-scm-presets' ), SCM_SCRIPTS_VERSION, false );
+            wp_register_script( 'jquery-scm-functions', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.functions.js', array( 'jquery-scm-presets' ), null, false );
             wp_enqueue_script( 'jquery-scm-functions' );
             
-            wp_register_script( 'jquery-scm-plugins', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.plugins.js', array( 'jquery-scm-functions' ), SCM_SCRIPTS_VERSION, false );
+            wp_register_script( 'jquery-scm-plugins', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.plugins.js', array( 'jquery-scm-functions' ), null, false );
             wp_enqueue_script( 'jquery-scm-plugins' );
 
-            wp_register_script( 'jquery-scm-tools', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.tools.js', array( 'jquery-scm-plugins' ), SCM_SCRIPTS_VERSION, false );
+            wp_register_script( 'jquery-scm-tools', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.tools.js', array( 'jquery-scm-plugins' ), null, false );
             wp_enqueue_script( 'jquery-scm-tools' );
             
             // jQuery Effects Core
@@ -114,16 +121,16 @@
 
             // Skip Link Focus Fix
 
-            wp_register_script( 'skip-link-focus-fix', SCM_ASSETS_URI_ASSETS . 'skip-link-focus-fix.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'skip-link-focus-fix', SCM_ASSETS_URI_ASSETS . 'skip-link-focus-fix.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'skip-link-focus-fix' );
 
             
             // jQuery Transform
 
-            wp_register_script( 'jquery-transform-2d', SCM_ASSETS_URI_ASSETS . 'jquery.transform/jquery.transform2d.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-transform-2d', SCM_ASSETS_URI_ASSETS . 'jquery.transform/jquery.transform2d.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'jquery-transform-2d' );
 
-            /*wp_register_script( 'jquery-transform-3d', SCM_ASSETS_URI_ASSETS . 'jquery.transform/jquery.transform3d.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, true );
+            /*wp_register_script( 'jquery-transform-3d', SCM_ASSETS_URI_ASSETS . 'jquery.transform/jquery.transform3d.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'jquery-transform-3d' );*/
 
             // jQuery Mobile
@@ -155,7 +162,7 @@
 
             // Fancybox
 
-            if( scm_field( 'opt-tools-fancybox', 0, 'option' ) ){
+            if( get_field( 'opt-tools-fancybox', 'option' ) ){
                 wp_register_script( 'fancybox', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/jquery.fancybox.pack.js', array( 'jquery' ), null, true );
                 wp_register_script( 'fancybox-thumbs', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/helpers/jquery.fancybox-thumbs.js', array( 'jquery' ), null, true );
                 wp_register_script( 'fancybox-buttons', SCM_ASSETS_URI_ASSETS . 'fancybox-2.1.5/helpers/jquery.fancybox-buttons.js', array( 'jquery' ), null, true );
@@ -168,8 +175,12 @@
 
             // Parallax Scrolling
 
-            if( scm_field( 'opt-tools-parallax', 0, 'option' ) ){
-                wp_register_script( 'parallax',  SCM_ASSETS_URI_ASSETS . 'parallax-1.4.2/parallax.min.js', array( 'jquery' ), null, true );
+            if( get_field( 'opt-tools-parallax', 'option' ) ){
+                $old = apply_filters( 'scm_assets_filter_parallax_131', false );
+                if(!$old)
+                    wp_register_script( 'parallax',  SCM_ASSETS_URI_ASSETS . 'parallax-1.4.2/parallax.min.js', array( 'jquery' ), null, true );
+                else
+                    wp_register_script( 'parallax',  SCM_ASSETS_URI_ASSETS . 'parallax-1.3.1/parallax.min.js', array( 'jquery' ), null, true );
                 wp_enqueue_script( 'parallax' );
             }
 
@@ -196,43 +207,44 @@
 
             wp_register_script( 'waypoints',  SCM_ASSETS_URI_ASSETS . 'waypoints-4.0.0/lib/jquery.waypoints.min.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'waypoints' );
-            //wp_register_script( 'waypoints-debug',  SCM_ASSETS_URI_ASSETS . 'waypoints-4.0.0/lib/waypoints.debug.js', false, SCM_SCRIPTS_VERSION, true );
+            //wp_register_script( 'waypoints-debug',  SCM_ASSETS_URI_ASSETS . 'waypoints-4.0.0/lib/waypoints.debug.js', false, SCM_ASSETS_VERSION, true );
             //wp_enqueue_script( 'waypoints-debug' );
             // import waypoints shortcuts if needed (sticky, infinite, ...)
 
             // SCM Child
 
-            wp_register_script( 'jquery-scm-child', SCM_URI_JS_CHILD . 'jquery.scm-child.js', array( 'jquery-scm-tools' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm-child', SCM_URI_JS_CHILD . 'jquery.scm-child.js', array( 'jquery-scm-tools' ), null, true );
             wp_enqueue_script( 'jquery-scm-child' );
             
             // SCM
 
-            wp_register_script( 'jquery-scm', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.scm.js', array( 'jquery-scm-child' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.scm.js', array( 'jquery-scm-child' ), null, true );
             wp_enqueue_script( 'jquery-scm' );
 
-            consoleLog(!is_admin());
         }
     }
 
+    // admin
     if ( ! function_exists( 'scm_assets_admin_register' ) ) {
         function scm_assets_admin_register() {
 
-            wp_register_script( 'jquery-scm-admin-child', SCM_URI_JS_CHILD . 'jquery.admin.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm-admin-child', SCM_URI_JS_CHILD . 'jquery.admin.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'jquery-scm-admin-child' );
 
-            wp_register_script( 'jquery-scm-admin', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.admin.js', array( 'jquery-scm-admin-child' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm-admin', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.admin.js', array( 'jquery-scm-admin-child' ), null, true );
             wp_enqueue_script( 'jquery-scm-admin' );
 
         }
     }
 
+    // login
     if ( ! function_exists( 'scm_assets_login_register' ) ) {
         function scm_assets_login_register() {
                 
-            wp_register_script( 'jquery-scm-login-child', SCM_URI_JS_CHILD . 'jquery.login.js', array( 'jquery' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm-login-child', SCM_URI_JS_CHILD . 'jquery.login.js', array( 'jquery' ), null, true );
             wp_enqueue_script( 'jquery-scm-login-child' );
             
-            wp_register_script( 'jquery-scm-login', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.login.js', array( 'jquery-scm-login-child' ), SCM_SCRIPTS_VERSION, true );
+            wp_register_script( 'jquery-scm-login', SCM_ASSETS_URI_ASSETS . 'jquery.scm/jquery.login.js', array( 'jquery-scm-login-child' ), null, true );
             wp_enqueue_script( 'jquery-scm-login' );                
 
         }
