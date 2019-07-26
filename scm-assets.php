950ecc5e771880dece41a4659972a626db16530d
Francesco Mazzola <info@mdmbunny.com>
2019-07-26 18:42:34 +0200

    v2.6.8

--- scm-assets.php
+++ scm-assets.php
@@ -3,7 +3,7 @@
  * Plugin Name:         SCM Assets
  * Plugin URI:          http://studiocreativo-m.it/
  * Description:         SCM Javascript Integration
- * Version:             2.6.7
+ * Version:             2.6.8
  * Author:              Studio Creativo M
  * Author URI:          http://studiocreativo-m.it/
  * License:             http://www.gnu.org/licenses/gpl-3.0.html
@@ -370,7 +370,7 @@
     if ( ! function_exists( 'scm_assets_register_fontawesome' ) ) {
         function scm_assets_register_fontawesome() {
 
-            wp_register_style('font-awesome', SCM_ASSETS_URI_ASSETS . 'fontawesome-pro-5.8.0-web/css/all.min.css', false, null );
+            wp_register_style('font-awesome', SCM_ASSETS_URI_ASSETS . 'fontawesome-pro-5.10.0-11-web/css/all.min.css', false, null );
             wp_enqueue_style( 'font-awesome' );
 
         }
